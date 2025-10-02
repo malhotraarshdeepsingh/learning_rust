9af49cd451cb2b08d8c2d2b580894ac3464dff0d
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-10-02 20:47:09 +0530

    Add project folders into main repo

---
+++ compress/src/main.rs
@@ -0,0 +1,25 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+use std::env::args;
+
+fn main(){
+    if args().len() != 3 {
+        eprintln!("Usage: {} <source> <destination>", args().next().unwrap());
+        return;
+    }
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!("Elapsed time: {:?}", start.elapsed());
+    println!("Original size: {} bytes", input.get_ref().metadata().unwrap().len());
+    println!("Compressed size: {} bytes", output.metadata().unwrap().len());
+}
---
+++ csv_file_reader/src/main.rs
@@ -0,0 +1,18 @@
+use std::error::Error;
+
+use csv;
+
+fn main(){
+    if let Err(err) = read_csv("./username.csv") {
+        eprintln!("{}", err);
+    }
+}
+
+fn read_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(file_path)?;
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
